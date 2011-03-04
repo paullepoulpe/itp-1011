c39c15e2f91c07d0dd62d2082d5b619e46bdba42
msap <maarten1709@users.noreply.github.com>
2011-03-04 10:21:22 +0000

    Metainfo marche encore mieux ;)

--- src/test/Metainfo.java
+++ src/test/Metainfo.java
@@ -18,7 +18,7 @@ import bencoding.InvalidBEncodingException;
 
 public class Metainfo {
 	public static void main(String[] args) {
-		File file = new File("data/LePetitPrince.torrent");
+		File file = new File("data/Plus44.torrent");
 		BDecoder bob = null;
 		BEValue dico = null, infoBEValue = null;
 		Map maHashTable = null, infoMap = null, a2 = null;
@@ -28,6 +28,7 @@ public class Metainfo {
 		try {
 			bob = new BDecoder(new FileInputStream(file));
 			dico = bob.bdecodeMap();
+			System.out.println(dico);
 		} catch (IOException e) {
 			System.out.println(e.getLocalizedMessage());
 		}
@@ -50,7 +51,7 @@ public class Metainfo {
 				announceList  = (ArrayList<BEValue>)mInfo[8];
 				for(int i=0;i<announceList.size();i++){
 					for(int j=0;j<announceList.get(i).getList().size();j++){
-						aList = aList+"\t"+announceList.get(i).getList().get(j).getString()+"\n";
+						aList = aList+"\t\t"+announceList.get(i).getList().get(j).getString()+"\n";
 					}
 				}
 				mInfo[8]=aList;
@@ -66,15 +67,17 @@ public class Metainfo {
 		String[] metaInfo = new String[9];
 		 Date date = new Date(((Long)mInfo[2]) * 1000);
 		 mInfo[2] = date.toString();
-		 mInfo[0] = "Tracker : " + mInfo[0];
-		 mInfo[1] = "Author : " + mInfo[1];
+		 mInfo[0] = "Tracker : \t" + mInfo[0];
+		 mInfo[1] = "Author : \t" + mInfo[1];
 		 mInfo[2] = "Creation Date : " + mInfo[2];
-		 mInfo[3] = "\nComment : " + mInfo[3];
-		 mInfo[4] = "\nFilename: " + mInfo[4];
-		 mInfo[5] = "SHA length: " + mInfo[5]+" Bytes";
-		 mInfo[6] = "PieceLength: " + mInfo[6] + " Bytes";
-		 mInfo[7] = "Size: " + mInfo[7] + " Bytes";
-		 mInfo[8] = "Tracker list: \n"+mInfo[8];
+		 mInfo[3] = "\nComment : \t" + mInfo[3];
+		 mInfo[4] = "\nFilename: \t" + mInfo[4];
+		 mInfo[5] = "SHA length: \t" + mInfo[5]+" Bytes";
+		 mInfo[6] = "PieceLength: \t" + mInfo[6] + " Bytes";
+		 mInfo[7] = "Size: \t\t" + mInfo[7] + " Bytes";
+		 if(!mInfo[8].equals("")){
+			 mInfo[8] = "Tracker list: \n"+mInfo[8];
+		 }
 		
 		 for (int i = 0; i < mInfo.length; i++) {
 		 System.out.println(mInfo[i]);
