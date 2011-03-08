e7dc974543573b4f97d44366920ee3469ce988dc
msap <maarten1709@users.noreply.github.com>
2011-03-08 10:34:30 +0000

    j'ai essayé de piger comment ca marchait... bref je suis pas sur que certains blocs soient dans les bonnes méthodes

--- src/http/HTTPGet.java
+++ src/http/HTTPGet.java
@@ -5,11 +5,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 
 import bencoding.BDecoder;
@@ -23,21 +26,49 @@ public class HTTPGet {
 	private int port;
 	private int left;
 	private byte compact;
+	private Socket socket;
 	private int numWant;
 	private String event;
 	private String trackerId;
+	private String query;
 
-	public HTTPGet() {
-		//a faire: initialiser tous les parametres, la plupart sont par default, a regarder dans le cours, 
-		//infoHash a l'air complique (voir digestInputStream dans BDecoder)
-		//perrId et TrackerId, je sais pas comment faire
+	public HTTPGet(String URLAnnounce) {
+		URL tracker = null;
+		String path, domain;
+		int serverPort;
+		try {
+			tracker = new URL(URLAnnounce);
+		} catch (MalformedURLException f) {
+			System.out
+					.println("L'url fournie en param�tre n'est pas dans un bon format.");
+		}
+		path = tracker.getPath();
+		domain = tracker.getHost();
+		serverPort = tracker.getPort();
+		try {
+			socket = new Socket(domain, serverPort);
+		} catch (UnknownHostException e) {
+		} catch (IOException f) {
+		}
+		infoHash = BinaryURLEncoder.encode(infoHash.getBytes());
+//		pour la methode qui suit, je suis pas sur que si un des parametres est null, cela marche quand meme
+//		il faudrait mettre des if() etc, mais ca m'a l'air vraiment long pour un truc aussi petit.
+		query = "GET" + path + "?info_hash=" + this.infoHash + "&peer_id="
+				+ this.peerId + "&port=" + port + "&left=" + this.left
+				+ "&compact=" + this.compact + "&event="+this.event+"HTTP/1.0\n\r\n\r\n";
+
+		// a faire: initialiser tous les parametres, la plupart sont par
+		// default, a regarder dans le cours,
+		// infoHash a l'air complique (voir digestInputStream dans BDecoder)
+		// perrId et TrackerId, je sais pas comment faire
 	}
 
 	public byte[] get() {
-		// a faire: créer un socket pour se connecter au tracker
-		//creer un streamwriter sur ce socket(voir cours)
-		//ecrire la requete sur le socket en suivant le modèle donnée dans le cours
-		//recuperer la reponse avec un inputStream (?sous forme de tableau de bytes??)
+		BufferedWriter request= null;
+		try{
+			request = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			request.write(this.query);
+		}catch(IOException e){}
 		return null;
 	}
 }
--- src/http/TrackerInfo.java
+++ src/http/TrackerInfo.java
@@ -7,7 +7,7 @@ package http;
 
 public class TrackerInfo {
 	public AnnounceInfo announce() {
-		// crée un objet Announce info pour un certain tracker
+		
 		return null;
 	}
 }
