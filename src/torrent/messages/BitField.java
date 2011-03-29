1dd1967e3349c96b01c7eb89adf4a02fa6f359eb
msap <maarten1709@users.noreply.github.com>
2011-03-29 14:30:16 +0000

    J'ai un peu modifie les fichier messages. .. loin d'etre fini ^^

--- src/torrent/messages/BitField.java
+++ src/torrent/messages/BitField.java
@@ -1,5 +1,9 @@
 package torrent.messages;
 
-public class BitField extends Message{
+/*
+ * Ce message correspond a un tableau de bits de taille egale au nombre de pieces du torrent.
+ * Les bits a 1 sont ceux que le pair possede.
+ */
+public class BitField extends Message {
 
 }
