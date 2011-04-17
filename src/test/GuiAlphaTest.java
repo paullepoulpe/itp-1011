7c4ee5209a84d435c12fd64d1966a75777f03d8e
msap <maarten1709@users.noreply.github.com>
2011-04-17 18:17:38 +0000

    Juste changé le GuiAlphaTest.java

--- src/test/GuiAlphaTest.java
+++ src/test/GuiAlphaTest.java
@@ -10,6 +10,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import torrent.Torrent;
 import torrent.peer.Peer;
@@ -23,24 +26,40 @@ class GuiAlpha extends JFrame implements Runnable {
 	public GuiAlpha() {
 		setPreferredSize(new Dimension(1280, 720));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		myTorrent = new Torrent(new File("data/Friday_RB.torrent"));
+		myTorrent = new Torrent(new File("data/G6.torrent"));
 		this.setTitle("Torrent : "+myTorrent.getMetainfo().getFileName());
 		tabs = new JTabbedPane();
 		tabInfo = new JPanel();
 		tabTracker = new JPanel();
 		tabInfo.setLayout(new BorderLayout());
-		info = new JTextArea(myTorrent.getMetainfo().toString());
+		info = new JTextArea(30, 50);
+		info.setText(myTorrent.getMetainfo().toString());
 		peers = new JTextArea(10,30);
-		peers.append("TEST POUR VOIR SI QQCH S'AFFICHE");
+//		peers.append("TEST POUR VOIR SI QQCH S'AFFICHE");
 		tabs.setTabPlacement(JTabbedPane.LEFT);
 		JScrollPane bob = new JScrollPane(peers);
-		bob.setSize(300,300);
-		peers.setSize(300,300);
 		bob.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		JScrollPane jane = new JScrollPane(info);
+		jane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		tabTracker.add(bob);
-		tabInfo.add(info,BorderLayout.CENTER);
+		tabInfo.add(jane,BorderLayout.CENTER);
 		tabs.addTab("Info", tabInfo);
-		tabs.addTab("Tracker", tabTracker);
+		tabs.addTab("Peers", tabTracker);
+		try {
+			UIManager.setLookAndFeel(UIManager.getInstalledLookAndFeels()[3]
+					.getClassName());
+			SwingUtilities.updateComponentTreeUI(tabs);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		
 		getContentPane().add(tabs);
 		pack();
 		setVisible(true);
